e595ffb377f6e5ea046c1c3bddee1900067308e2
safwanvk <safwanvalakundil@gmail.com>
2020-08-10 23:48:53 +0530

    :white_check_mark: Fetch bio

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from selenium import webdriver
 from config import USERNAME, PASSWORD
 import time
 
+users = ['__she._.rin__']
+
 browser = webdriver.Chrome(executable_path='/home/safwan/chromedriver_linux64/chromedriver')
 
 browser.maximize_window()
-browser.get('http://www.instgram.com')
+browser.get('https://www.instagram.com')
 
 time.sleep(2)
 
@@ -17,3 +19,20 @@ password.send_keys(PASSWORD)
 
 login_btn = browser.find_element_by_css_selector('button[type="submit"]')
 login_btn.click()
+
+time.sleep(2)
+
+for user in users:
+    browser.get(f"https://www.instagram.com/{user}/")
+
+    time.sleep(2)
+
+    number_of_posts, followers, following = browser.find_elements_by_css_selector(
+        '.g47SY')
+    print('Number of Posts:', number_of_posts.text, 'Followers:', followers.text, 'Following:', following.text)
+
+    time.sleep(1)
+
+    bio = browser.find_element_by_css_selector(
+        '.-vDIg')
+    print(bio.text)
